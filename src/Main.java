0e3e2bcc86d3b30fef4226e278fd409fcef16e78
Val-0612 <diegovcg1@gmail.com>
2026-04-15 09:58:39 -0700

    Se realizó el cuarto método

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 void main() {
     ArrayList<String> lista = new ArrayList<>(Arrays.asList(
             "ajolote", "hermano", "ajo","Diego"));
     System.out.println("  Antes  : " + lista);
     metodosStrings.filtroSelectivo(lista, 'a', 5);
     System.out.println("  Después: " + lista);
-
     System.out.println(metodosStrings.convertidorAMayusculas(lista));
+
+    ArrayList<Integer> numeros = new ArrayList<>();
+    for (int i=0; i<10; i++){
+        numeros.add(i);
+        numeros.add(i);
+    }
+
+    System.out.println(metodosInt.cuadradosUnicos(numeros));
+
+
 }
--- src/metodosInt.java
+++ src/metodosInt.java
@@ -1,10 +1,19 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.stream.Collectors;
 
 public class metodosInt {
 
     //Método 1
     public static void elMultiplicador(ArrayList<Integer> lista, int factor){
         lista.replaceAll(n->n*factor);
+    }
+
+    //Método 4
+    public static HashSet<Integer> cuadradosUnicos(ArrayList<Integer> lista){
+        HashSet<Integer> cuadradosUnicos= (HashSet<Integer>) lista.stream().map(n->n*n).collect(Collectors.toSet());
+        return cuadradosUnicos;
+
     }
 
 
